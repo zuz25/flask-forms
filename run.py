3ca86f362c0459a9a317c04b0913c2875bcc34d3
Bryan Hicks <zuz25@byu.net>
2015-05-21 13:33:09 -0500

    modified form displayed

--- run.py
+++ run.py
@@ -5,42 +5,55 @@ from flask_wtf import Form, RecaptchaField
 from flask_wtf.file import FileField
 from wtforms import TextField, HiddenField, ValidationError, RadioField,\
     BooleanField, SubmitField, IntegerField, FormField, validators
-from wtforms.validators import Required
+from wtforms.validators import Required, NumberRange
 import requests
+from flask.ext.appconfig import AppConfig
 
-#class ExampleForm(Form):
-#    field1 = TextField('First Field', description='This is field one.')
-#    field2 = TextField('Second Field', description='This is field two.',
-#                       validators=[Required()])
-#    hidden_field = HiddenField('You cannot see this', description='Nope')
-#    recaptcha = RecaptchaField('A sample recaptcha field')
-#    radio_field = RadioField('This is a radio field', choices=[
-#        ('head_radio', 'Head radio'),
-#        ('radio_76fm', "Radio '76 FM"),
-#        ('lips_106', 'Lips 106'),
-#        ('wctr', 'WCTR'),
-#    ])
-#    checkbox_field = BooleanField('This is a checkbox',
-#                                  description='Checkboxes can be tricky.')
-#
-#    # subforms
-#    mobile_phone = FormField(TelephoneForm)
-#
-#    # you can change the label as well
-#    office_phone = FormField(TelephoneForm, label='Your office phone')
-#
-#    ff = FileField('Sample upload')
-#
-#    submit_button = SubmitField('Submit Form')
-#
-#
-#    def validate_hidden_field(form, field):
-#        raise ValidationError('Always wrong')
 
-#class Access(Form):
-#    client_id = TextField('Client ID',[validators.required()])
-#    client_secret = TextField('Client Secret',[validators.required()])
+class TelephoneForm(Form):
+    country_code = IntegerField('Country Code', [validators.required()])
+    area_code = IntegerField('Area Code/Exchange', [validators.required()])
+    number = TextField('Number')
+    
+    
+class ExampleForm(Form):
+    url = TextField('URL', description='Enter URL to send recordings',validators=[Required()])
+    user = TextField('Username', description='Enter Username for URL to support Basic Authentication',validators=[Required()])
+    password = TextField('Password', description='Enter Password for URL to support Basic  Authentication',validators=[Required()])
+    beep = FileField('Sample upload')
+    interval = IntegerField('Seconds between beeps', description='Enter the number of seconds between beeps 12-15s',validators=[NumberRange(min=12, max=15)])
+    #hidden_field = HiddenField('You cannot see this', description='Nope')
+    ##recaptcha = RecaptchaField('A sample recaptcha field')
+    #radio_field = RadioField('This is a radio field', choices=[
+    #    ('head_radio', 'Head radio'),
+    #    ('radio_76fm', "Radio '76 FM"),
+    #    ('lips_106', 'Lips 106'),
+    #    ('wctr', 'WCTR'),
+    #])
+    #checkbox_field = BooleanField('This is a checkbox', description='Checkboxes can be tricky.')
+    #
+    ## subforms
+    #mobile_phone = FormField(TelephoneForm)
+    #
+    ## you can change the label as well
+    #office_phone = FormField(TelephoneForm, label='Your office phone')
 
+    
+
+    submit_button = SubmitField('Submit Form')
+
+    def validate_hidden_field(form, field):
+        raise ValidationError('Always wrong')
+
+#def register(request):
+#    form = ExampleForm(request.POST)
+#    if request.method == 'POST' and form.validate():
+#        formData = formData()
+#        formData.field1=form.field1.data
+#        formData.field2=form.field2.data
+#        formData.save()
+#        redirect('register')
+#    return render_response('register.html', form=form)
 
 def create_app(configfile=None):
     app = Flask(__name__)
@@ -51,21 +64,31 @@ def create_app(configfile=None):
 
     # in a real app, these should be configured through Flask-Appconfig
     app.config['SECRET_KEY'] = 'devkey'
-    app.config['RECAPTCHA_PUBLIC_KEY'] = \
-        '6Lfol9cSAAAAADAkodaYl9wvQCwBMr3qGR_PPHcw'
+    #app.config['RECAPTCHA_PUBLIC_KEY'] = \
+    #    '6Lfol9cSAAAAADAkodaYl9wvQCwBMr3qGR_PPHcw'
 
     @app.route('/', methods=('GET', 'POST'))
     def index():
-        form = Access()
-        form.validate_on_submit()  # to get error messages to the browser
-        flash('critical message', 'critical')
-        flash('error message', 'error')
-        flash('warning message', 'warning')
-        flash('info message', 'info')
-        flash('debug message', 'debug')
-        flash('different message', 'different')
-        flash('uncategorized message')
-f        return render_template('index.html', form=form, respStatus=respStatus,access_token=access_token)
+        form = ExampleForm()
+        if form.validate_on_submit():
+            appSettings=appSettings()
+            appSettings.url=form.url.data
+            appSettings.password=form.password.data
+            appSettings.user=form.user.data
+            appSettings.interval=form.interval.data
+            appSettings.url=form.url.data
+            appSettings.url=form.url.data
+            # to get error messages to the browser
+        else:
+            print(form.errors)
+        #flash('critical message', 'critical')
+        #flash('error message', 'error')
+        #flash('warning message', 'warning')
+        #flash('info message', 'info')
+        #flash('debug message', 'debug')
+        #flash('different message', 'different')
+        #flash('uncategorized message')
+        return render_template('index.html', form=form)
     return app
 
 if __name__ == '__main__':
