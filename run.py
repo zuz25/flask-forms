f6be659927ecc80e7512fd99e65c9d4ae789ecde
Bryan Hicks <linds@Bryans-MacBook.local>
2015-05-29 21:30:17 -0500

    Added new fields and form validation

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ from flask_appconfig import AppConfig
 from flask_wtf import Form, RecaptchaField
 from flask_wtf.file import FileField
 from wtforms import TextField, HiddenField, ValidationError, RadioField,\
-    BooleanField, SubmitField, IntegerField, FormField, validators
-from wtforms.validators import Required, NumberRange
+    BooleanField, SubmitField, IntegerField, FormField, validators, PasswordField
+from wtforms.validators import Required, NumberRange, URL, Email, EqualTo
 import requests
 from flask.ext.appconfig import AppConfig
 import os
@@ -17,55 +17,37 @@ UPLOAD_FOLDER = "MCRconfigs"
 
 ALLOWED_EXTENSIONS = set(['mp3','wav'])
 
-class TelephoneForm(Form):
-    country_code = IntegerField('Country Code', [validators.required()])
-    area_code = IntegerField('Area Code/Exchange', [validators.required()])
-    number = TextField('Number')
     
     #"C:\Users\bh680n\Documents\scripts\LearnForms\MCRconfigs"
 class ExampleForm(Form):
-    url = TextField('URL', description='Enter URL to send recordings',validators=[Required()])
+    apiAdminEmail = TextField('Email', description='Enter API Admin Email (Existing or desired)', validators=[Required(),Email(message=u'Invalid Email address')])
+    applicationName = TextField('Name of Call Recording Configuration', description='Enter a name for the call recording configuration')
+    url = TextField('URL', description='Enter URL to send recordings',validators=[Required(),URL(require_tld=True, message=u'Invalid URL')])
     user = TextField('Username', description='Enter Username for URL to support Basic Authentication',validators=[Required()])
-    password = TextField('Password', description='Enter Password for URL to support Basic  Authentication',validators=[Required()])
-    beep = FileField('Beep File', description='.mp3 or .wav file to be played at a specified interval throughout the call',validators=[Required()])
+    password = PasswordField('Password', description='Enter Password for URL to support Basic  Authentication',validators=[Required(),EqualTo('confirm',message=u'Passwords must match')])
+    confirm = PasswordField('Confirm Password', description='Confirm Password for URL to support Basic  Authentication',validators=[Required()])
+    notification = FileField('Initial Announcement', description='.mp3 or .wav file of a notification to be played at start of call')
+    beep = FileField('Beep File', description='.mp3 or .wav file to be played at a specified interval throughout the call')
     interval = IntegerField('Seconds between beeps', description='Enter the number of seconds between beeps 12-15s',validators=[NumberRange(min=12, max=15)])
-    #hidden_field = HiddenField('You cannot see this', description='Nope')
-    ##recaptcha = RecaptchaField('A sample recaptcha field')
-    #radio_field = RadioField('This is a radio field', choices=[
-    #    ('head_radio', 'Head radio'),
-    #    ('radio_76fm', "Radio '76 FM"),
-    #    ('lips_106', 'Lips 106'),
-    #    ('wctr', 'WCTR'),
-    #])
-    #checkbox_field = BooleanField('This is a checkbox', description='Checkboxes can be tricky.')
-    #
-    ## subforms
-    #mobile_phone = FormField(TelephoneForm)
-    #
-    ## you can change the label as well
-    #office_phone = FormField(TelephoneForm, label='Your office phone')
-
-    
+    endCall = FileField('Beep File', description='.mp3 or .wav file to be played at the end of 90 mins to inform callers that call will end')
 
     submit_button = SubmitField('Submit Form')
 
     def validate_hidden_field(form, field):
         raise ValidationError('Always wrong')
 
-#def register(request):
-#    form = ExampleForm(request.POST)
-#    if request.method == 'POST' and form.validate():
-#        formData = formData()
-#        formData.field1=form.field1.data
-#        formData.field2=form.field2.data
-#        formData.save()
-#        redirect('register')
-#    return render_response('register.html', form=form)
-
 def exists_directory(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
     return directory
+
+# def upload_file(file):
+#     if file.has_file and allowed_file(file.data.filename):
+#         exists_directory(UPLOAD_FOLDER)
+#         directory = exists_directory(os.path.join(UPLOAD_FOLDER,applicationName))
+#         open(os.path.join(directory,file.data.filename), 'w').write(file.data.filename)
+#         print(file.data.filename,"Saved")
+#         return file.data.filename
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -94,6 +76,7 @@ def create_app(configfile=None):
             beep=form.beep
             print(url+"\n"+password+"\n"+user+"\n"+str(interval))
             print(beep.data.filename)
+            
             if beep.has_file and allowed_file(beep.data.filename):
                 print("If statement entered")
                 exists_directory(UPLOAD_FOLDER)
